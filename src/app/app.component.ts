bddb3fcf428e9ae13ffe887a60a125887111f65f
EdwinCDones <edwin.condor.1997@gmail.com>
2018-06-27 19:28:00 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  lado: number;
+  base: number;
+  altura: number;
+  radio: number;
+  respuesta: number;
+  pi: 3.14;
+
+  cuadrado() {
+    this.respuesta = this.lado * this.lado;
+  }
+
+  rectangulo() {
+    this.respuesta = this.base * this.altura;
+  }
+  triangulo() {
+    this.respuesta = this.base * this.altura / 2;
+  }
+  circulo() {
+    this.respuesta = this.pi * this.radio;
+  }
+
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ChartsModule } from 'ng2-charts';
 import { AppComponent } from './app.component';
+import { FormsModule } from "@angular/forms";
+import { LineasComponent } from './components/lineas/lineas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LineasComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
